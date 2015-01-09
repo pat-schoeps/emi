35e4fff3749295ccf1d9667b97392a2b24ef36c4
pschoeps <schoeps.pat@gmail.com>
2015-01-09 00:17:22 +0000

    mobile

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'performance'     => 'static_pages#performance'
   get 'about'           => 'static_pages#about'
   get 'contact'         => 'static_pages#contact'
+  get 'experimental'    => 'static_pages#experimental'
   get 'signup'          => 'users#new'
   resources :users
 
